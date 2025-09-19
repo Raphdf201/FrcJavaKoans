eef8134903343df399b9ac117f40c5087ad4c793
Raphaël Deschênes <raphdf201@hotmail.com>
2025-09-19 15:22:59 -0400

    Reorder koans

--- src/main/java/sensei/Wisdom.java
+++ src/main/java/sensei/Wisdom.java
@@ -10,13 +10,13 @@ import engine.Koan;
 public final class Wisdom {
     public static final List<List<Koan>> koans = List.of(
         AboutConsoleAndVariablesKoans.koans,
-        AboutMethodsKoans.koans,
-        AboutConditionsKoans.koans,
-        AboutMoreMethodsKoans.koans,
         AboutDecimalNumbersKoans.koans,
+        AboutConditionsKoans.koans,
         AboutLoopsKoans.koans,
-        AboutNot7GameKoans.koans,
+        AboutMethodsKoans.koans,
+        AboutMoreMethodsKoans.koans,
         AboutClassesKoans.koans,
-        AboutObjectsKoans.koans
+        AboutObjectsKoans.koans,
+        AboutNot7GameKoans.koans        
     );
 }
